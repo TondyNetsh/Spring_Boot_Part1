cc06f43fb798f6d328a1fdb997f6cfd9ed64cd1e
Rotondwa Netshimbupfe <59872053+TondyNetsh@users.noreply.github.com>
2020-07-24 16:56:35 +0200

    Delete FakeRepoInterface.java

--- src/main/java/FakeRepoInterface.java
+++
@@ -1,9 +0,0 @@
-import com.Springboot.User;
-public abstract interface FakeRepoInterface <T>{
-    public T insertUser(User user);
-
-    Object insertUser(Object user);
-
-    public T findUserById(long id);
-    public T deleteUser(long id);
-}
